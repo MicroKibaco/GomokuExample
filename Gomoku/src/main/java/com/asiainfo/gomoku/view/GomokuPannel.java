5e8d50ff4c491a12660e8b913df2148440e85474
MicroKibaco <yangzy3@asiainfo.com>
2017-02-10 20:45:47 +0800

    绘制棋盘

---
+++ Gomoku/src/main/java/com/asiainfo/gomoku/view/GomokuPannel.java
@@ -0,0 +1,158 @@
+package com.asiainfo.gomoku.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.asiainfo.gomoku.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 作者:小木箱 邮箱:yangzy3@asiainfo.com 创建时间:2017年02月10日16点59分 描述:五子棋盘自定义view
+ */
+public class GomokuPannel extends View {
+
+    private int mPanelWidth;
+    private float mLineHeight;
+    private int MAX_LINE = 10;
+    private float ratioPieceOfLineHight = 3 * 1.0f / 4;
+
+    private Bitmap mWhitePiece;
+    private Bitmap mBlackPiece;
+
+    private Paint mPaint = new Paint();
+    private List<Point> mWhiteArray;
+    private List<Point> mBlackArray;
+
+    public GomokuPannel(Context context) {
+        this(context, null);
+
+    }
+
+    private void initView() {
+
+        mWhitePiece = BitmapFactory.decodeResource(getResources(), R.drawable.stone_w2);
+        mBlackPiece = BitmapFactory.decodeResource(getResources(), R.drawable.stone_b1);
+        mWhiteArray = new ArrayList<>();
+        mBlackArray = new ArrayList<>();
+        setBackgroundColor(0x00ff0000);
+        mPaint.setColor(0x99ff0000);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+
+    }
+
+    public GomokuPannel(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+        initView();
+    }
+
+    public GomokuPannel(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+
+        int heighSize = MeasureSpec.getSize(heightMeasureSpec);
+        int heighMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        int width = Math.min(widthSize, heighSize);
+
+        if (widthMode == MeasureSpec.UNSPECIFIED) {
+            width = heighSize;
+        } else if (heighMode == MeasureSpec.UNSPECIFIED) {
+
+            width = widthSize;
+
+        }
+        setMeasuredDimension(width, width);
+
+    }
+
+    /**
+     * 描述:对尺寸相关的成员变量进行赋值 创建时间:2/10/17/20:38 作者:小木箱 邮箱:yangzy3@asiainfo.com
+     */
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        mPanelWidth = w;
+        mLineHeight = mPanelWidth * 1.0f / MAX_LINE;
+
+        int pieceWidth = (int) (mLineHeight * ratioPieceOfLineHight);
+
+        mWhitePiece = Bitmap.createScaledBitmap(mWhitePiece, pieceWidth, pieceWidth, false);
+        mBlackPiece = Bitmap.createScaledBitmap(mBlackPiece, pieceWidth, pieceWidth, false);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        drawBoard(canvas);
+    }
+
+    /**
+     * 描述:绘制棋盘 创建时间:2/10/17/20:40 作者:小木箱 邮箱:yangzy3@asiainfo.com
+     */
+
+    private void drawBoard(Canvas canvas) {
+
+        int w = mPanelWidth;
+        float lineHeight = mLineHeight;
+
+        for (int i = 0; i < MAX_LINE; i++) {
+
+            //画横线
+            int startX = (int) (lineHeight / 2);
+            int endX = (int) (w - lineHeight / 2);
+
+            //画纵坐标
+            int y = (int) ((0.5 + i) * lineHeight);
+
+            canvas.drawLine(startX, y, endX, y, mPaint);
+
+            canvas.drawLine(y, startX, y, endX, mPaint);
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = event.getAction();
+
+        switch (action) {
+
+            case MotionEvent.ACTION_DOWN:
+
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                break;
+
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+
+
+        return true;
+
+    }
+}
